f004b17e5f15a897e5b58c51f0256436790bb7ef
Dhnillhaq <yakulhaq@gmail.com>
2024-11-08 00:11:56 +0700

    refactor : rename models file

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -2,7 +2,7 @@
 class Admin extends Controller{
     public function index(){
         $this->view('Admin/index');
-        $this->model('Prestasi_model')->getAllPrestasi();
+        $this->model('Prestasi')->getAllPrestasi();
     }
 }
 ?>
