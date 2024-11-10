91710e23b6c3c3db44de25dbd6ad12e12e9eb5f6
Dhnillhaq <yakulhaq@gmail.com>
2024-11-10 13:51:22 +0700

    refactor : Perbaikan Struktur

--- app/controllers/Kajur.php
+++ app/controllers/Kajur.php
@@ -1,7 +1,12 @@
-<?php 
-class Kajur extends Controller{
-    public function index($nama="Dhanil"){
+<?php
+class Kajur extends Controller
+{
+    public function index($nama = "Dhanil")
+    {
+        $this->view('templates/header');
         $this->view('Kajur/index', $nama);
+        $this->view('templates/footer');
+
     }
 }
 ?>
--- app/controllers/Umum.php
+++ app/controllers/Umum.php
@@ -1,7 +1,12 @@
 <?php
 class Umum extends Controller {
     public function index(){
+        
+        $this->view('templates/header');
         $this->view('Umum/index');
+        $this->view('templates/footer');
+        
+        
     }
 }
 
