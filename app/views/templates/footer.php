a04cdff545321aac4b1a86dc624aef14545369c8
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2024-11-29 15:42:54 +0700

    feat: fixed dupe and typo

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,9 +1,10 @@
 <footer class="bg-[#132145] text-white p-4 font-[poppins]">
-        <div class="container mx-auto flex justify-end">
-            <p class="font-semibold">
-                2024 &copy; ArchieveIT
-            </p>
-        </div>
-    </footer>
+    <div class="container mx-auto flex justify-end">
+        <p class="font-semibold">
+            2024 &copy; AchieveIT
+        </p>
+    </div>
+</footer>
 </body>
+
 </html>
