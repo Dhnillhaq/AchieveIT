9ec69615adacb83714f45185c1f0b01ebaa0911d
restuprtma <restu.prtma05@gmail.com>
2024-12-09 16:41:39 +0700

    feat: regestrasi sama pagination

--- app/views/Admin/profilAdmin.php
+++ app/views/Admin/profilAdmin.php
@@ -141,30 +141,35 @@
 			<!-- pagination -->
         <section class="flex items-center justify-center py-2">
   <nav aria-label="Page navigation example">
-    <ul class="pagination bg-blue-50 p-3 rounded-lg border-blue-500">
-      <li class="page-item">
-        <a class="page-link bg-blue-50 hover:bg-blue-100" href="#" aria-label="Previous">
-          <span aria-hidden="true">&laquo;</span>
-          <span class="sr-only">Previous</span>
-        </a>
-      </li>
-      <li class="page-item">
-        <a class="page-link bg-blue-50 hover:bg-blue-100" href="#">1</a>
-      </li>
-      <li class="page-item">
-        <a class="page-link bg-blue-50 hover:bg-blue-100" href="#">2</a>
-      </li>
-      <li class="page-item">
-        <a class="page-link bg-blue-50 hover:bg-blue-100" href="#">3</a>
-      </li>
-      <li class="page-item">
-        <a class="page-link bg-blue-50 hover:bg-blue-100" href="#" aria-label="Next">
-          <span aria-hidden="true">&raquo;</span>
-          <span class="sr-only">Next</span>
-        </a>
-      </li>
-    </ul>
-  </nav>
+  <ul class="flex items-center -space-x-px h-8 text-sm">
+    <li>
+      <a href="#" class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-blue-600 border border-e-0 border-gray-300 rounded-s-lg hover:bg-blue-200 hover:text-blue-700">
+        <span class="sr-only">Previous</span>
+        <svg class="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+        </svg>
+      </a>
+    </li>
+    <li>
+      <a href="#" aria-current="page" class="z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-gray-300 bg-transparent hover:bg-blue-200 hover:text-blue-700">1</a>
+    </li>
+    <li>
+      <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-gray-300 bg-transparent hover:bg-blue-200 hover:text-blue-700">2</a>
+    </li>
+    <li>
+      <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-gray-300 bg-transparent hover:bg-blue-200 hover:text-blue-700">3</a>
+    </li>
+    <li>
+      <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-gray-300 rounded-e-lg bg-transparent hover:bg-blue-200 hover:text-blue-700">
+        <span class="sr-only">Next</span>
+        <svg class="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+        </svg>
+      </a>
+    </li>
+  </ul>
+</nav>
+
 </section>
 
 		</div>
