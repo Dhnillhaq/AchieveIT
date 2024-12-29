b70c83a6cce771c8656839e4317f85596371e2d1
restuprtma <restu.prtma05@gmail.com>
2024-12-29 21:23:16 +0700

    feat: responsive lama (sebelum iir bikin)

--- app/views/Mahasiswa/index.php
+++ app/views/Mahasiswa/index.php
@@ -1,178 +1,165 @@
-<section class=" sm:ml-64 bg-blue-50 min-h-screen">
-	<!-- profil -->
-	<?php require_once __DIR__ .'/../templates/profiles.php'; ?>
+<section class="sm:ml-64 bg-blue-50 min-h-screen p-4">
+    <!-- profil -->
+    <?php require_once __DIR__ .'/../templates/profiles.php'; ?>
 
-	<!-- selamat datang -->
-	<section class="flex-col justify-start p-6 space-y-4">
-		<p class="font-bold text-4xl">Selamat Datang</p>
-		<p class="font-semibold text-2xl text-[#F99D1C]">
-			<?= $data['mhs']['0']['nama'] ?> / <?= $data['mhs']['0']['nim'] ?>
-	</section>
+    <!-- selamat datang -->
+    <section class="flex-col justify-start p-4 space-y-4">
+        <p class="font-bold text-2xl sm:text-3xl md:text-4xl">Selamat Datang</p>
+        <p class="font-semibold text-xl sm:text-2xl text-[#F99D1C]">
+            <?= $data['mhs']['0']['nama'] ?> / <?= $data['mhs']['0']['nim'] ?>
+        </p>
+    </section>
 
-			<!-- prestasi -->
-			<section class="flex justify-start p-6 space-x-10 ">
-				<!-- total prestasi -->
-				<div class="bg-white p-4 rounded-lg shadow-lg border w-1/4">
-					<div class="flex justify-start space-x-4">
-						<img src="../../../public/img/Total_Prestasi.png" alt="logo" class="w-auto h-12">
-						<div class="flex-col">
-							<p class="font-semibold text-[#757575] text-[12px]">Total Prestasi Saat Ini</p>
-							<p class="font-bold"><?= $data['mhs']['1']['total_prestasi'] ?></p>
+    <!-- prestasi -->
+    <section class="flex flex-col sm:flex-row justify-start p-4 space-y-4 sm:space-y-0 sm:space-x-4">
+        <!-- total prestasi -->
+        <div class="bg-white p-4 rounded-lg shadow-lg border w-full sm:w-1/3">
+            <div class="flex justify-start space-x-4 items-center">
+                <img src="../../../public/img/Total_Prestasi.png" alt="logo" class="w-12 h-12">
+                <div class="flex-col">
+                    <p class="font-semibold text-[#757575] text-xs">Total Prestasi Saat Ini</p>
+                    <p class="font-bold text-base"><?= $data['mhs']['1']['total_prestasi'] ?></p>
+                </div>
+            </div>
+        </div>
+
+        <!-- total poin -->
+        <div class="bg-white p-4 rounded-lg shadow-lg border w-full sm:w-1/3">
+            <div class="flex justify-start space-x-4 items-center">
+                <img src="../../../public/img/Total_poin.png" alt="logo" class="w-12 h-12">
+                <div class="flex-col">
+                    <p class="font-semibold text-[#757575] text-xs">Total Poin Saat Ini</p>
+                    <p class="font-bold text-base"><?= $data['mhs']['1']['total_poin'] ?></p>
+                </div>
+            </div>
+        </div>
+
+        <!-- peringkat mapres -->
+        <div class="bg-white p-4 rounded-lg shadow-lg border w-full sm:w-1/3">
+            <div class="flex justify-start space-x-4 items-center">
+                <img src="../../../public/img/Perankingan_Mhs.png" alt="logo" class="w-12 h-12">
+                <div class="flex-col">
+                    <p class="font-semibold text-[#757575] text-xs">Peringkat MaPres</p>
+                    <p class="font-bold text-base"><?= $data['mhs']['1']['peringkat_mapres'] ?></p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="p-4">
+        <h1 class="text-2xl sm:text-3xl md:text-4xl font-bold">Daftar Mahasiswa Berprestasi</h1>
+    </section>
+
+    <!-- table maPres -->
+    <section id="daftar-prestasi">
+        <div class="p-4">
+        	<div class="flex flex-col sm:flex-row justify-between space-y-4 sm:space-y-0 w-full">
+				<!-- Search -->
+				<div class="flex w-full sm:w-auto items-center space-x-4">
+					<form id="formFilter" action="<?= BASEURL; ?>/Mahasiswa" method="POST" class="flex-1">
+						<div class="flex items-center bg-white p-2 space-x-1 rounded-md border shadow-md focus-within:ring-2 focus-within:ring-blue-500">
+							<img src="../../../public/img/Search (1).png" alt="logo" class="w-5 h-5">
+							<input type="text" id="cari-mhs" placeholder="Cari Mahasiswa" class="w-full flex focus:outline-none" name="keyword" />
 						</div>
-					</div>
 				</div>
 
-				<!-- total poin -->
-				<div class="bg-white p-4 rounded-lg shadow-lg border w-1/4">
-					<div class="flex justify-start space-x-4">
-						<img src="../../../public/img/Total_poin.png" alt="logo" class="w-auto h-12">
-						<div class="flex-col">
-							<p class="font-semibold text-[#757575] text-[12px]">Total Poin Saat Ini</p>
-							<p class="font-bold"><?= $data['mhs']['1']['total_poin'] ?></p>
-						</div>
+				<!-- Filters -->
+				<div class="flex flex-row items-center justify-end w-full sm:space-x-4 space-x-2">
+					<!-- Limit Filter -->
+					<div class="flex items-center">
+						<span class="hidden sm:inline">Lihat</span>
+						<select name="limit" onchange="submitForm()" 
+							class="mx-2 border rounded-lg px-2 py-1 text-sm bg-white shadow-gray-400 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
+							<option value="10" selected>10</option>
+							<option value="20">20</option>
+							<option value="50">50</option>
+						</select>
+						<span class="hidden sm:inline">entri</span>
 					</div>
+
+					<!-- Year Filter -->
+					<div class="flex items-center">
+						<select name="year" onchange="submitForm()" 
+							class="border rounded-lg px-2 py-1 text-sm bg-white shadow-gray-400 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
+							<option value="2025">2022</option>
+							<option value="2026">2023</option>
+							<option value="2024" selected>2024</option>
+						</select>
+					</div>
+					</form>
 				</div>
+		</div>
 
-				<!-- peringkat mapres -->
-				<div class="bg-white p-4 rounded-lg shadow-lg border w-1/4">
-					<div class="flex justify-start space-x-4">
-						<img src="../../../public/img/Perankingan_Mhs.png" alt="logo" class="w-auto h-12">
-						<div class="flex-col">
-							<p class="font-semibold text-[#757575] text-[12px]">Peringkat MaPres</p>
-							<p class="font-bold"><?= $data['mhs']['1']['peringkat_mapres'] ?></p>
-						</div>
-					</div>
-				</div>
-			</section>
 
-			<section class="p-6 justify-start">
-				<h1 class="text-4xl font-bold ">Daftar Mahasiswa Berprestasi</h1>
-			</section>
+            <!-- Table -->
+            <div class="mt-6 overflow-x-auto bg-white shadow-md rounded-2xl">
+                <table class="min-w-full bg-white">
+                    <thead>
+                        <tr>
+                            <th class="py-2 px-4 bg-blue-950 text-white font-semibold text-left border border-blue-950 hidden sm:table-cell">RANKING</th>
+                            <th class="py-2 px-4 bg-blue-950 text-white font-semibold text-left border border-blue-950 hidden sm:table-cell">NIM</th>
+                            <th class="py-2 px-4 bg-blue-950 text-white font-semibold text-left border border-blue-950">NAMA MAHASISWA</th>
+                            <th class="py-2 px-4 bg-blue-950 text-white font-semibold text-left border border-blue-950 hidden md:table-cell">PRODI</th>
+                            <th class="py-2 px-4 bg-blue-950 text-white font-semibold text-left border border-blue-950">TOTAL POIN</th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-gray-700">
+                        <?php
+                        $rank = 1;
+                        foreach ($data['prestasi'] as $mahasiswa) {
+                            echo "<tr>";
+                            echo "<td class='py-2 px-4 border border-blue-950 hidden sm:table-cell'>$rank</td>";
+                            echo "<td class='py-2 px-4 border border-blue-950 hidden sm:table-cell'>{$mahasiswa['nim']}</td>";
+                            echo "<td class='py-2 px-4 border border-blue-950'>{$mahasiswa['nama_mahasiswa']}</td>";
+                            echo "<td class='py-2 px-4 border border-blue-950 hidden md:table-cell'>{$mahasiswa['prodi']}</td>";
+                            echo "<td class='py-2 px-4 border border-blue-950'>{$mahasiswa['total_poin']}</td>";
+                            echo "</tr>";
+                            $rank++;
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
 
-			<!-- table maPres -->
-			<!-- dari Components -->
-			<section id="daftar-prestasi" class="">
-				<div class="p-5 mb-10 ">
-					<div class="flex justify-between">
-						<div class="flex">
-							<form id="formFilter" action="<?= BASEURL; ?>/Mahasiswa" method="POST">
-								<div class="flex items-center bg-white w-full p-2 space-x-1 rounded-md border shadow-md focus-within:ring-2 focus-within:ring-blue-500">
-									<img src="../../../public/img/Search (1).png" alt="logo" class="w-5 h-5">
-									<input type="text" id="cari-mhs" placeholder="" class="w-full flex focus:outline-none" name="keyword"/>
-								</div>
-
-						</div>
-						<div class="flex right-0">
-							<div class="flex items-center mr-3">
-
-								<span class="">Lihat</span>
-								<select name="limit" onchange="submitForm()"
-									class="mx-2 border rounded-lg px-2 py-1 text-sm bg-white shadow-gray-400 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
-									<option value="10" selected>10</option>
-									<option value="20">20</option>
-									<option value="50">50</option>
-								</select>
-
-								<span class="">entri</span>
-								<select name="year" onchange="submitForm()"
-									class="right-0 mx-2 border rounded-lg px-2 py-1 text-sm bg-white shadow-gray-400 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
-									<option value="2025">2022</option>
-									<option value="2026">2023</option>
-									<option value="2024" selected>2024</option>
-								</select>
-
-								</form>
-							</div>
-						</div>
-					</div>
-
-					<div class="mt-10 overflow-x-auto bg-white shadow-md rounded-2xl">
-						<table class="min-w-full bg-white">
-							<thead>
-								<tr>
-									<th
-										class="py-2 px-4 bg-blue-950 text-white font-semibold text-left border border-blue-950">
-										RANKING
-									</th>
-									<th
-										class="py-2 px-4 bg-blue-950 text-white font-semibold text-left border border-blue-950">
-										NIM
-									</th>
-									<th
-										class="py-2 px-4 bg-blue-950 text-white font-semibold text-left border border-blue-950">
-										NAMA MAHASISWA
-									</th>
-									<th
-										class="py-2 px-4 bg-blue-950 text-white font-semibold text-left border border-blue-950">
-										PRODI
-									</th>
-									<th
-										class="py-2 px-4 bg-blue-950 text-white font-semibold text-left border border-blue-950">
-										TOTAL POIN
-									</th>
-								</tr>
-							</thead>
-							<tbody class="text-gray-700">
-								<?php
-
-								// Looping data mahasiswa ke dalam tabel
-								$rank = 1;
-								foreach ($data['prestasi'] as $mahasiswa) {
-									echo "<tr>";
-									echo "<td class='py-2 px-4 border border-blue-950'>$rank</td>";
-									echo "<td class='py-2 px-4 border border-blue-950'>{$mahasiswa['nim']}</td>";
-									echo "<td class='py-2 px-4 border border-blue-950'>{$mahasiswa['nama_mahasiswa']}</td>";
-									echo "<td class='py-2 px-4 border border-blue-950'>{$mahasiswa['prodi']}</td>";
-									echo "<td class='py-2 px-4 border border-blue-950'>{$mahasiswa['total_poin']}</td>";
-									echo "</tr>";
-									$rank++;
-								}
-
-								?>
-							</tbody>
-						</table>
-					</div>
-					<section class="flex items-center justify-center py-2">
-  <nav aria-label="Page navigation example">
-  <ul class="flex items-center -space-x-px h-8 text-sm">
-    <li>
-      <a href="#" class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-blue-600 border border-e-0 border-gray-300 rounded-s-lg hover:bg-blue-200 hover:text-blue-700">
-        <span class="sr-only">Previous</span>
-        <svg class="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-        </svg>
-      </a>
-    </li>
-    <li>
-      <a href="#" aria-current="page" class="z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-gray-300 bg-transparent hover:bg-blue-200 hover:text-blue-700">1</a>
-    </li>
-    <li>
-      <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-gray-300 bg-transparent hover:bg-blue-200 hover:text-blue-700">2</a>
-    </li>
-    <li>
-      <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-gray-300 bg-transparent hover:bg-blue-200 hover:text-blue-700">3</a>
-    </li>
-    <li>
-      <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-gray-300 rounded-e-lg bg-transparent hover:bg-blue-200 hover:text-blue-700">
-        <span class="sr-only">Next</span>
-        <svg class="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-        </svg>
-      </a>
-    </li>
-  </ul>
-</nav>
-
-</section>
-				</div>
-			</section>
-			<!-- //php include __DIR__ . '/../../components/DaftarMahasiswaBerprestasi.php';  -->
-	</section>
+            <!-- Pagination -->
+            <section class="flex items-center justify-center py-4">
+                <nav aria-label="Page navigation">
+                    <ul class="flex items-center -space-x-px h-8 text-sm">
+                        <li>
+                            <a href="#" class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-blue-600 border border-e-0 border-gray-300 rounded-s-lg hover:bg-blue-200 hover:text-blue-700">
+                                <span class="sr-only">Previous</span>
+                                <svg class="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                                </svg>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" aria-current="page" class="z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-gray-300 bg-transparent hover:bg-blue-200 hover:text-blue-700">1</a>
+                        </li>
+                        <li>
+                            <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-gray-300 bg-transparent hover:bg-blue-200 hover:text-blue-700">2</a>
+                        </li>
+                        <li>
+                            <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-gray-300 bg-transparent hover:bg-blue-200 hover:text-blue-700">3</a>
+                        </li>
+                        <li>
+                            <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-gray-300 rounded-e-lg bg-transparent hover:bg-blue-200 hover:text-blue-700">
+                                <span class="sr-only">Next</span>
+                                <svg class="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </section>
+        </div>
+    </section>
 </section>
 
 <!-- Function js untuk submit form select ketika opsi yang lain dipilih -->
 <script>
-	function submitForm() {
-		document.getElementById("formFilter").submit();
-	}
+function submitForm() {
+    document.getElementById("formFilter").submit();
+}
 </script>
--- app/views/templates/sidebar.php
+++ app/views/templates/sidebar.php
@@ -45,14 +45,21 @@ foreach ($activePaths as $path) {
 // Tentukan kelas aktif
 $activeClass = $isActive ? 'text-[#FEC01A] bg-[#3063C559]' : 'text-white hover:bg-[#3063C559]';
 $imageName = $isActive ? 'Administrasi-data(1)' : 'Administrasi-data';
+?>
 
 
-if ($_SESSION['user']['role'] == "Super Admin" || $_SESSION['user']['role'] == "Admin") { ?>
-    <!-- sidebar -->
-    <aside id="default-sidebar"
-        class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
-        aria-label="Sidebar">
-        <div class="h-full px-3 py-4 overflow-y-auto bg-[#132145]">
+<?php if ($_SESSION['user']['role'] == "Super Admin" || $_SESSION['user']['role'] == "Admin") { ?>
+    <!-- Mobile Toggle Button -->
+    <button id="sidebar-toggle" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-blue-500 rounded-lg sm:hidden hover:bg-[#3063C559] focus:outline-none focus:ring-2 focus:ring-gray-200">
+        <span class="sr-only">Open sidebar</span>
+        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+        </svg>
+    </button>
+
+    <!-- Sidebar -->
+    <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 bg-[#132145] sm:block" aria-label="Sidebar">
+        <div class="h-full px-3 py-4 overflow-y-auto">
             <ul class="space-y-2 font-medium">
                 <li>
                     <div class="flex items-center justify-center text-4xl p-4 text-white rounded-lg ">
@@ -101,13 +108,43 @@ if ($_SESSION['user']['role'] == "Super Admin" || $_SESSION['user']['role'] == "
             </ul>
         </div>
     </aside>
-    <?php
-}
-if ($_SESSION['user']['role'] == "Ketua Jurusan") { ?>
+
+    <!-- JavaScript for Mobile Sidebar Toggle -->
+    <script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const sidebarToggle = document.getElementById('sidebar-toggle');
+        const sidebar = document.getElementById('default-sidebar');
+
+        sidebarToggle.addEventListener('click', function() {
+            sidebar.classList.toggle('-translate-x-full');
+        });
+
+        // Close sidebar when clicking outside on mobile
+        document.addEventListener('click', function(event) {
+            const isClickInsideSidebar = sidebar.contains(event.target);
+            const isClickOnToggle = sidebarToggle.contains(event.target);
+            
+            if (!isClickInsideSidebar && !isClickOnToggle && window.innerWidth < 640) {
+                sidebar.classList.add('-translate-x-full');
+            }
+        });
+    });
+    </script>
+<?php } ?>
+    
+
+<?php if ($_SESSION['user']['role'] == "Ketua Jurusan") { ?>
+    <!-- Mobile Toggle Button -->
+    <button id="sidebar-toggle" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-white rounded-lg sm:hidden hover:bg-[#3063C559] focus:outline-none focus:ring-2 focus:ring-gray-200">
+        <span class="sr-only">Open sidebar</span>
+        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+        </svg>
+    </button>
+
     <!-- sidebar -->
-    <aside id="default-sidebar"
-        class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
-        aria-label="Sidebar">
+    <aside id="default-sidebar" 
+        class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 bg-[#132145]  sm:block" aria-label="Sidebar">
         <div class="h-full px-3 py-4 overflow-y-auto bg-[#132145]">
             <ul class="space-y-2 font-medium">
                 <li>
@@ -143,13 +180,41 @@ if ($_SESSION['user']['role'] == "Ketua Jurusan") { ?>
         </div>
     </aside>
 
-    <?php
-}
+    <!-- JavaScript for Mobile Sidebar Toggle -->
+    <script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const sidebarToggle = document.getElementById('sidebar-toggle');
+        const sidebar = document.getElementById('default-sidebar');
 
-if ($_SESSION['user']['role'] == "Mahasiswa") { ?>
+        sidebarToggle.addEventListener('click', function() {
+            sidebar.classList.toggle('-translate-x-full');
+        });
+
+        // Close sidebar when clicking outside on mobile
+        document.addEventListener('click', function(event) {
+            const isClickInsideSidebar = sidebar.contains(event.target);
+            const isClickOnToggle = sidebarToggle.contains(event.target);
+            
+            if (!isClickInsideSidebar && !isClickOnToggle && window.innerWidth < 640) {
+                sidebar.classList.add('-translate-x-full');
+            }
+        });
+    });
+    </script>
+<?php } ?>
+
+<?php if ($_SESSION['user']['role'] == "Mahasiswa") { ?>
+    <!-- Mobile Toggle Button -->
+    <button id="sidebar-toggle" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-blue-500 rounded-lg sm:hidden hover:bg-[#3063C559] focus:outline-none focus:ring-2 focus:ring-gray-200">
+        <span class="sr-only">Open sidebar</span>
+        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+        </svg>
+    </button>
+
     <!-- sidebar -->
-    <aside id="default-sidebar"
-        class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+    <aside id="default-sidebar" 
+        class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 bg-[#132145] sm:block" 
         aria-label="Sidebar">
         <div class="h-full px-3 py-4 overflow-y-auto bg-[#132145]">
             <ul class="space-y-2 font-medium">
@@ -194,6 +259,25 @@ if ($_SESSION['user']['role'] == "Mahasiswa") { ?>
         </div>
     </aside>
 
-    <?php
-}
-?>
+    <!-- JavaScript for Mobile Sidebar Toggle -->
+    <script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const sidebarToggle = document.getElementById('sidebar-toggle');
+        const sidebar = document.getElementById('default-sidebar');
+
+        sidebarToggle.addEventListener('click', function() {
+            sidebar.classList.toggle('-translate-x-full');
+        });
+
+        // Close sidebar when clicking outside on mobile
+        document.addEventListener('click', function(event) {
+            const isClickInsideSidebar = sidebar.contains(event.target);
+            const isClickOnToggle = sidebarToggle.contains(event.target);
+            
+            if (!isClickInsideSidebar && !isClickOnToggle && window.innerWidth < 640) {
+                sidebar.classList.add('-translate-x-full');
+            }
+        });
+    });
+    </script>
+<?php } ?>
